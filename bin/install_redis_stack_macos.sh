e6cc67a8b6d88336b9355f7a674293dc93178541
ArachisTong <152833721+ArachisTong@users.noreply.github.com>
2024-03-19 22:31:41 +0800

    Create install_redis_stack_macos.sh

---
+++ bin/install_redis_stack_macos.sh
@@ -0,0 +1,11 @@
+# 下载源码
+brew tap redis-stack/redis-stack
+
+# 安装
+brew install redis-stack
+
+# 设置环境变量
+echo "export PATH=/usr/local/Caskroom/redis-stack-server/7.2.0-v9/bin:$PATH">> ~/.bashrc
+
+# start server
+nohup redis-stack-server >redis-stack-server.log &
